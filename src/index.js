c45cb24f133c7e6949c8c9eda58d18561cd808e5
AlphaBroke <bennihuehn@googlemail.com>
2020-11-05 18:20:17 +0100

    fix distance

--- src/index.js
+++ src/index.js
@@ -33,28 +33,32 @@ const navElements = [
     color: 'yellow'
   }
 ]
-
+// let = local var but reset on render boese
+// useState = local state triggers rerender, constant over component lifetime
+// useRef = local var dont trigger rerender, constant over component lifetime
 function Viewpager() {
   const index = useRef(0)
   const [props, set] = useSprings(navElements.length, (i) => ({ x: i * window.innerWidth, sc: 1, display: 'block' }))
   const bind = useGesture(({ down, delta: [xDelta], direction: [xDir], distance, cancel }) => {
     const newX = (i) => (i - index.current) * window.innerWidth + (down ? xDelta : 0)
     const isEdge = (i) => i < index.current - 1 || i > index.current + 1
+    const centerX = window.innerWidth / 2
+    console.log(xDir)
 
     if (down) {
       set((i) => {
-        if (isEdge(i)) return { display: 'none' }
+        // if (isEdge(i)) return { display: 'none' }
         return { x: newX(i), display: 'block', immediate: true }
       })
-    } else if (!down && distance > window.innerWidth / 2) {
+    } else if (!down && Math.abs(xDelta) > centerX) {
       index.current = clamp(index.current + (xDir > 0 ? -1 : 1), 0, navElements.length - 1)
       set((i) => {
-        if (isEdge(i)) return { display: 'none' }
+        // if (isEdge(i)) return { display: 'none' }
         return { x: newX(i), display: 'block', immediate: false }
       })
-    } else if (!down && distance <= window.innerWidth / 2) {
+    } else if (!down && Math.abs(xDelta) <= centerX) {
       set((i) => {
-        if (isEdge(i)) return { display: 'none' }
+        // if (isEdge(i)) return { display: 'none' }
         return { x: newX(i), display: 'block', immediate: false }
       })
     }
