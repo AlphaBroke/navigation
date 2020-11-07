0f027262bbdb417a17623bc2bba84a00c6c2d849
AlphaBroke <bennihuehn@googlemail.com>
2020-11-07 19:17:09 +0100

    simulate gesture on click

--- src/index.js
+++ src/index.js
@@ -41,10 +41,10 @@ function Viewpager() {
   const [props, set] = useSprings(navElements.length, (i) => ({ x: i * window.innerWidth, sc: 1, display: 'block' }))
 
   const bind = useGesture(({ down, delta: [xDelta], velocity }) => {
-    ani(down, xDelta, velocity)
+    animatePageTransition(down, xDelta, velocity)
   })
 
-  function ani(down, xDelta, velocity) {
+  function animatePageTransition(down, xDelta, velocity) {
     const newX = (i) => (i - index.current) * window.innerWidth + (down ? xDelta : 0)
     const isEdge = (i) => i < index.current - 1 || i > index.current + 1
     const centerX = window.innerWidth / 2
@@ -82,11 +82,11 @@ function Viewpager() {
   }
 
   function Back() {
-    ani(false, 1, 1.01)
+    animatePageTransition(false, 1, 1.01)
   }
 
   function Forward() {
-    ani(false, -1, 1.01)
+    animatePageTransition(false, -1, 1.01)
   }
 
   return (
